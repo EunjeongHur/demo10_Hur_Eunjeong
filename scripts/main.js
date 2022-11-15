8f7a57212650ac235f36075ea6bde5dce3ea5651
EunjeongHur <ungdunge2@gmail.com>
2022-11-15 12:44:43 -0800

    complete demo11

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);   //global
+
+        // the following functions are always called when someone is logged in
+        read_display_Quote();
+        insertName();
+        populateCardsDynamically();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+
+
 function read_display_Quote(){
     //console.log("inside the function")
 
@@ -8,52 +25,67 @@ function read_display_Quote(){
         document.getElementById("quote-goes-here").innerHTML=tuesdayDoc.data().quote;
     })
 }
-read_display_Quote();
 
-function insertName(){
-// to check if the user is logged in:
- firebase.auth().onAuthStateChanged(user =>{
-     if (user){
-         console.log(user.uid); // let me to know who is the user that logged in to get the UID
-        currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
-        currentUser.get().then(userDoc=>{
-            //get the user name
-            var user_Name= userDoc.data().name;
-            console.log(user_Name);
-            $("#name-goes-here").text(user_Name); //jquery
-            // document.getElementByID("name-goes-here").innetText=user_Name;
-        })    
-    }
 
- })
+// Insert name function using the global variable "currentUser"
+function insertName() {
+    currentUser.get().then(userDoc => {
+        //get the user name
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        $("#name-goes-here").text(user_Name); //jquery
+        // document.getElementByID("name-goes-here").innetText=user_Name;
+    })
 }
-insertName();
+
 
 function populateCardsDynamically() {
     let hikeCardTemplate = document.getElementById("hikeCardTemplate");
     let hikeCardGroup = document.getElementById("hikeCardGroup");
     
-    db.collection("hikes").get()
+    db.collection("hikes")
+        .orderBy("length").limit(2).get()
         .then(allHikes => {
             allHikes.forEach(doc => {
                 var hikeName = doc.data().name; //gets the name field
                 var hikeID = doc.data().code; //gets the unique ID field
                 var hikeLength = doc.data().length; //gets the length field
                 let testHikeCard = hikeCardTemplate.content.cloneNode(true);
-                console.log(hikeID);
+                // console.log(hikeID);
                 testHikeCard.querySelector('.card-title').innerHTML = hikeName;     //equiv getElementByClassName
                 testHikeCard.querySelector('.card-length').innerHTML = hikeLength;  //equiv getElementByClassName
                 testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);//equiv getElementByTagName
+
+                testHikeCard.querySelector('i').id = 'save-' + hikeID;
+                // this line will call a function to save the hikes to the user's document             
+                testHikeCard.querySelector('i').onclick = () => saveBookmark(hikeID);
+
                 testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;   //equiv getElementByTagName
+                testHikeCard.querySelector('.read-more').href = "eachHike.html?hikeName="+hikeName +"&id=" + hikeID;
                 hikeCardGroup.appendChild(testHikeCard);
             })
 
         })
 }
-populateCardsDynamically();
+
 
 function setHikeData(id){
-    console.log(id);
+    // console.log(id);
     localStorage.setItem('hikeID', id)
+}
+
+function saveBookmark(id){
+    console.log(id, "icon clicked")
+    currentUser.set({
+        bookmarks: firebase.firestore.FieldValue.arrayUnion(id)
+    }, {
+        merge: true
+    })
+    .then( function(){
+        console.log("This bookmark is for" + currentUser);
+        var iconID = 'save-'+id
+        console.log(iconID);
+        document.getElementById(iconID).innerText = 'bookmark';
+    })
 }
 
